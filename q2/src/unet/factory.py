2e0a1c4d26a67917f4ba690bfe188e8ae45684b8
Varun0157 <varun.edachali@gmail.com>
2025-03-31 11:48:07 +0530

    feat: no skip and residual unet models
    2.2 and 2.3

--- q2/src/unet/factory.py
+++ q2/src/unet/factory.py
@@ -3,13 +3,21 @@ from enum import Enum
 
 from src.unet.unet import UNet
 from src.unet.vanilla import VanillaUNet
+from src.unet.noskip import NoSkipUNet
+from src.unet.residual import ResidualUNet
 
 
 class Variant(Enum):
     Vanilla = "vanilla"
+    NoSkip = "noskip"
+    Residual = "residual"
 
 
 def fetch_unet(variant: Variant) -> type[UNet]:
     match variant:
         case Variant.Vanilla:
             return VanillaUNet
+        case Variant.NoSkip:
+            return NoSkipUNet
+        case Variant.Residual:
+            return ResidualUNet
